fe2ea3c9239d5768d1847cd755725b7dd8802370
kienp <kienp@KIEN-PC>
2024-10-26 07:00:15 +0700

    add checker

--- Easy Minecraft Server Gui/Program.cs
+++ Easy Minecraft Server Gui/Program.cs
@@ -1,3 +1,6 @@
+﻿using Krypton.Toolkit;
+using System.Diagnostics;
+
 namespace Easy_Minecraft_Server_Gui
 {
     internal static class Program
@@ -11,7 +14,19 @@ namespace Easy_Minecraft_Server_Gui
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            
+            if (File.Exists(Path.Combine(Share.AP,"Checker.tmp"))==false)
+            {
+                MessageBox.Show("Đây Là Lần Chạy Đầu Tiên Của Bạn, Vui Lòng Chờ Chúng Tôi Setup Cho Bạn...");
+                var output = Share.Command([ "zrok.exe disable", "zrok.exe enable ixd88rZAdaue"]);
+
+                if (!output.Contains("the zrok environment was successfully enabled"))
+                {
+                    MessageBox.Show("Lỗi, Vui Lòng Check File Log!");
+                    Process.Start(Path.Combine(Share.AP, "CMD_LOG.txt"));
+                    return;
+                }
+                File.Create(Path.Combine(Share.AP, "Checker.tmp"));
+            }
             Application.Run(new Form1());
         }
     }
